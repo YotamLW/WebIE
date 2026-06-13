c1c973746aaec2b579366061fc980de84765797d
DrMinotaur <18ylimor-waisberg@CMS.tela.org.uk>
2026-06-13 01:11:09 +0100

    latex-style maths typing mathjax package, notes on units / dimensional analysis

---
+++ website/mathjax/macros.js
@@ -0,0 +1,20 @@
+MathJax = {
+    assistive : {speech : false},
+    tex: {
+        inlineMath : [['$', '$']],
+        macros : {
+            // Unit formatting 
+            u : ['\\mkern2mu\\mathrm{#1}', 1],
+            // Common units 
+            um : '\\u{m}',
+            ukg : '\\u{kg}',
+            us : '\\u{s}',
+            uA : '\\u{A}',
+            uK : '\\u{K}',
+            mol : '\\u{mol}',
+            uN : '\\u{N}',
+            uJ : '\\u{J}',
+            uV : '\\u{V}',
+        }
+    }
+}
